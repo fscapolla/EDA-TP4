7fa347ee258c03d159c8d942d02a6174cc4e00f2
fscapolla <fscapolla@itba.edu.ar>
2020-04-10 12:05:35 -0300

    Últimos cambios

--- TP4/Dispatcher.cpp
+++ TP4/Dispatcher.cpp
@@ -48,14 +48,14 @@ void dispatch(Event ev, int wormCount, Worm * wArray[], Grapher * grapher)
 	case POSSIBLE_WORM_STOP:
 		for (int i = 0; i < wormCount; i++)		//Itera todos los worms
 		{
-			wArray[i]->update(ev);				//Actualiza propiedades de cada Worm
+			wormPtr[i]->update(ev);				//Actualiza propiedades de cada Worm
 		}
 		break;
 
 	case REFRESH:
 		for (int i = 0; i < wormCount; i++)		//Itera todos los worms
 		{
-			wArray[i]->update(ev);				//increment frameCount of worm.
+			wormPtr[i]->update(ev);				//increment frameCount of worm.
 			grapher->update(wArray, wormCount);	//Actualiza el display con la informacion de cada Worm
 		}
 		break;
